ca6ce2c82abe34d295ee5212c2cee6d5fbc2e409
plwdesign <128205620+plwdesign@users.noreply.github.com>
2024-04-25 15:01:00 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -281,7 +281,7 @@ system_node_install() {
   sleep 2
 
   sudo su - root <<EOF
-  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+  curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
   apt-get install -y nodejs
   sleep 2
   npm install -g npm@latest
